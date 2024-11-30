dce1f2fbe21319b66e6803d58cd6d65ae0fcf371
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2024-11-30 01:36:37 +0100

    chore(app): set up Express app with routes, delay middleware, and error handling

--- backend-service/src/app.ts
+++ backend-service/src/app.ts
@@ -1,1 +1,25 @@
-console.log("Main app");
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import todoRoute from "./routes/todoRoutes";
+import postRoute from "./routes/postRoutes";
+import { delayMiddleware } from "./middlewares/delayMiddleware";
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+app.use(delayMiddleware);
+
+app.use("/api/todos", todoRoute);
+app.use("/api/posts", postRoute);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+export default app;
